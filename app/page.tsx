4f002fc35045f845138e5e4387324eedeff2bf78
Md Anjum Kamali <mdanjumkamali@gmail.com>
2025-03-08 08:12:37 +0530

    Add Herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { HeroWithMockup } from "@/components/blocks/hero-with-mockup";
+
 export default function Home() {
-  return <h1>Hey</h1>;
+  return (
+    <div>
+      <HeroWithMockup
+        title="Hyperscale your Audit & Advisory firm with AI"
+        description="SignalsHQ improves the performance of Audit & Advisory firms with agents to collect, extract, analyse, verify and reference documentation at scale"
+        primaryCta={{
+          text: "Start Building",
+          href: "/signup",
+        }}
+        mockupImage={{
+          alt: "AI Platform Dashboard",
+          width: 1248,
+          height: 765,
+          src: "https://www.launchuicomponents.com/app-light.png",
+        }}
+      />
+    </div>
+  );
 }
